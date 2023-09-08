6f4025a24658517f27ceb51f25e37b5ea109d8fa
Marlon Couto <devpatola@proton.me>
2023-09-07 23:27:59 -0300

    Finaliza aplicação

---
+++ Data/Migrations/20230907233624_initialsetup.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,13 @@
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Question { get; set; } = "";
+        public string Answer { get; set; } = "";
+
+        public Joke()
+        {
+        }
+    }
+}
